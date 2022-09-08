9a4342324dd17b962ac8baa38cd15ada4240daea
Ethiopian_boy <mullerfromethiopia@gmail.com>
2022-09-08 20:55:32 +0530

    Thur, 9/8/2022

---
+++ LetterToYourself.java
@@ -0,0 +1,18 @@
+/*
+ a program that displays your name and address on the screen
+ as if it were a letter. */
+public class LetterToYourself {
+  public  static void main(String args[]) {
+    System.out.println("+-------------------------------------------+");
+    System.out.println("|                                      #### |");
+    System.out.println("|                                      #### |");
+    System.out.println("|                                      #### |");
+    System.out.println("|                                           |");
+    System.out.println("|                                           |");
+    System.out.println("|                    Mulubrhan Legesse      |");
+    System.out.println("|                    student, university    |");
+    System.out.println("|                    Rajkot, Gujarat,India  |");
+    System.out.println("|                                           |");
+    System.out.println("+-------------------------------------------+");
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Programming-by-Doing
 “The best way to learn is to do.” - P.R. Halmos
+Learn Java the Hard Way
+`https://www.programmingbydoing.com/`
