6712e91026215d4a58c26f694562607a27ae86c8
Ethiopian_boy <mullerfromethiopia@gmail.com>
2022-09-09 23:11:51 +0530

    MBL

---
+++ MyIntials.java
@@ -0,0 +1,25 @@
+public class MyIntials {
+  public static void main(String args[]) {
+    int i, j, k;
+    for (i=1;i<=7;i++) {
+        if(i==1) {
+          System.out.println("M   M  BBBB   L    ");
+          }
+        if(i==2 || i==3) {
+          System.out.println("MM MM  B   B  L    ");
+        }
+        if(i==4){
+          System.out.println("M M M  BBBB   L    ");
+        }
+        if(i==5){
+          System.out.println("M   M  B   B  L    ");
+        }
+        if(i==6){
+          System.out.println("M   M  B   B  L    ");
+        }
+        if(i==7){
+          System.out.println("M   M  BBBB   LLLLL");
+        }
+      }
+    }
+  }
